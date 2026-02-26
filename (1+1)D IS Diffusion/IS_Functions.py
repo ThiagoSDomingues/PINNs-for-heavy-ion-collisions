58948eb49829bfa5ab8f1e350d365d54e95cf2c9
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-02-26 14:16:13 -0300

    Configure device and define QCD parameters
    Added device configuration and QCD-like parameters.

--- (1+1)D IS Diffusion/IS_Functions.py
+++ (1+1)D IS Diffusion/IS_Functions.py
@@ -2,6 +2,11 @@
 
 import torch
 import torch.nn.functional as F
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 import os
 import numpy as np
+
+# QCD-like equation of state (Conformal (since ε = 3P))
+N_c = 3 # colors
+N_f = 3 # 3 flavors
