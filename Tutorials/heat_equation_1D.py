b6a451c9688b40455cc8e7d751787a0092adb4c9
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-02-26 01:45:45 -0300

    Add PINN class for 1D heat equation model

--- Tutorials/heat_equation_1D.py
+++ Tutorials/heat_equation_1D.py
@@ -5,3 +5,19 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 1. Define the Neural Network: we define a model as a class
+class PINN(nn.Module):
+    def __init__(self):
+        super(PINN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(2, 32), nn.Tanh(), # The input is 2 (Position $x$ and Time $t$). It outputs 32 hidden features;
+            nn.Linear(32, 32), nn.Tanh(), # Tanh is the "Activation Function." It’s a smooth curve;
+            nn.Linear(32, 32), nn.Tanh(),
+            nn.Linear(32, 1) # The final output is 1 (The temperature $u$ at that specific $x$ and $t$).
+        )
+
+    def forward(self, x, t):
+        # Concatenate x and t as input features
+        u = self.net(torch.cat([x, t], dim=1))
+        return u
+
