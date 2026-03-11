c4423dc394a17efc9ae268e9fdd02671377ef3d8
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-03-10 22:49:12 -0300

    Atualizar o Navier_Stokes.py

--- Tutorials/Navier_Stokes.py
+++ Tutorials/Navier_Stokes.py
@@ -1,17 +1,29 @@
 #!shebang
 """
-Script description: using PINNs to solve Navier-Stokes (non-relativistic) equations
+Script description: using PINNs to solve Navier-Stokes (non-relativistic) equations. Probably I will divide this script in several pieces to have a modular code. 
 """
+
 import torch 
+# import auto grad (automatic derivatives) 
 
 
-# usually we define a class for a PINN
+# usually we define a class for a PINN: input (space and time?) and output (physical quantities)
+
+# Neural Network Architecture: usually  multi-perceptron layers (MPL)
 class PINN:
 
     def __init__(self):
         self.attribute 
 
-def main: 
+    def function:
+        return result
+
+# training dataset: do we call it collocation points?
+
+# define the activation function here? 
+# ReLU, tanh, etc...
+
+def main:
    
 
    
