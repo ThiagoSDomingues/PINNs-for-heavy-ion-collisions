99235065195a8c92fc6b531909bfdf1d9c0c0477
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-03-10 22:36:54 -0300

    Criar Navier_Stokes.py

---
+++ Tutorials/Navier_Stokes.py
@@ -0,0 +1,19 @@
+#!shebang
+"""
+Script description: using PINNs to solve Navier-Stokes (non-relativistic) equations
+"""
+import torch 
+
+
+# usually we define a class for a PINN
+class PINN:
+
+    def __init__(self):
+        self.attribute 
+
+def main: 
+   
+
+   
+    
+   
