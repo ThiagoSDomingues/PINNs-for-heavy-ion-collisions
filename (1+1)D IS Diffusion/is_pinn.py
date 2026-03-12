95f64f473e496f2331cef626dad531c6f33866b2
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-03-12 15:39:53 -0300

    Update is_pinn.py

--- (1+1)D IS Diffusion/is_pinn.py
+++ (1+1)D IS Diffusion/is_pinn.py
@@ -1,1 +1,34 @@
+#!shebang
 
+"""
+Physics-Informed Neural Network (PINN) for Israel-Stewart diffusion: forward problem (evolve n(t,x) and J(t,x)). 
+The PINN must learn (n(t,x), J(t,x)) subject to local rest frame pde's for conserved current and relaxation time equation.
+Solves the (1+1)D Israel-Stewart diffusion system in the local rest frame (v=0, fixed uniform temperature).
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# ------------------
+# constants
+# ------------------
+
+Nc = 3 
+Nf = 3 
+T = 0.2 # constant temperature
+
+# ------------------
+# thermodynamics functions
+# ------------------
+
+def alpha_from_n(n):
+    return
+
+def sigma(alpha):
+    return
+    
+def tau_J(alpha,n):
+    return 12*sigma(alpha)*T/n
