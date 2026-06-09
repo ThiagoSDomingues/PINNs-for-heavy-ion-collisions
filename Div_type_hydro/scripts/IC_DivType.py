babd41caf66ba1d0e1b10c2388c7e2b76d7eb5fa
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-06-09 11:50:04 -0300

    Refactor initial conditions for divergence-type PINN

--- Div_type_hydro/scripts/IC_DivType.py
+++ Div_type_hydro/scripts/IC_DivType.py
@@ -1,27 +1,51 @@
+import numpy as np
 import torch
-from BDNK_IS_Functions import T_func, alpha_from_n_func
+from DivType_Functions import T_func, alpha_from_n_func
 
 def IC_DivType(x, L):
     """
     Initial conditions for the divergence‑type PINN.
-    Returns (q_ic, alpha_ic) at t=0.
-    Uses the same Gaussian ansatz as the BDNK‑IS runs.
+    Returns (scriptJ_ic, alpha_ic) at t=0.
+    Generates different setups.
     """
-    x = x.clone().detach().requires_grad_(True)
+    N = x.shape[0] 
+#    x = x.clone().detach().requires_grad_(True)
     t_vals = x[:, 0:1]
     x_vals = x[:, 1:2]
 
+    # Initial charge density n(t=0,x)
+    p, qq, r = 0.2, 7.0, 1.0    # Initial condition parameters
+    
+    # First setup:
+    n_init = p * torch.exp(- (qq * x_vals / L)**2) + r
+
+    # Second setup:
+    # sharpness = 60
+    # n_init = (1.1 - 0.1*torch.tanh(sharpness*((4*x_vals/L)**2-1)))
+
+    # Third setup:
+    #n_init = 1e-3*(p * torch.exp(- (qq * x_vals / L)**2) + r)
+
     T = T_func(t_vals, x_vals)
 
-    # Initial charge density n(0,x)
-    p, qq, r = 0.2, 7.0, 1.0   # qq to avoid conflict with the flux variable
-    n_init = (p * torch.exp(- (qq * x_vals / L)**2) + r) * 1e-2
-
     # Obtain alpha from the equation of state
-    alpha_init = alpha_from_n_func(n_init, T)
+    alpha_init = alpha_from_n_func(n_init)
 
-    # Initial diffusive flux q(0,x)
-    d, f, g = 0.05, 10.0, 1.05
-    q_init = (d * torch.exp(- (f * x_vals / L)**2) + g) * 1e-2
+#    dx = x_vals[1] - x_vals[0]
+#    alpha_padded = torch.cat([alpha_init[:1], alpha_init, alpha_init[-1:]], dim=0)
+#    N_x_init = -(alpha_padded[2:] - alpha_padded[:-2]) / (2.0 * dx)
+    
 
-    return q_init, alpha_init
+    # Initial diffusive flux scriptJ(t=0,x)
+    d, f, g = 0.05, 10.0, 1.05 # Initial condition parameters
+
+    # First setup:
+    scriptJ_init = d * torch.exp(- (f * x_vals / L)**2) + g
+
+    # Second setup:
+    #scriptJ_init = torch.ones_like(x_vals)*g
+
+    # Third setup:
+    #scriptJ_init = 1e-3 * torch.ones_like(x_vals)*g
+
+    return scriptJ_init, alpha_init
