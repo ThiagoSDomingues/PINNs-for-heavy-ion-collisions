1f2acf49883316fc946d1a8c632f172684ebdfb8
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-06-11 16:10:06 -0300

    Rename scriptJ to q in IC_DivType function

--- Div_type_hydro/scripts/IC_DivType.py
+++ Div_type_hydro/scripts/IC_DivType.py
@@ -5,7 +5,7 @@ from DivType_Functions import T_func, alpha_from_n_func
 def IC_DivType(x, L):
     """
     Initial conditions for the divergence‑type PINN.
-    Returns (scriptJ_ic, alpha_ic) at t=0.
+    Returns (q_ic, alpha_ic) at t=0.
     Generates different setups.
     """
     N = x.shape[0] 
@@ -26,26 +26,21 @@ def IC_DivType(x, L):
     # Third setup:
     #n_init = 1e-3*(p * torch.exp(- (qq * x_vals / L)**2) + r)
 
-    T = T_func(t_vals, x_vals)
+#    T = T_func(t_vals, x_vals) # in the future, if we want to generalize alpha and n we need to add a temperature dependence! 
 
-    # Obtain alpha from the equation of state
-    alpha_init = alpha_from_n_func(n_init)
+    # convert to fugacity using the EOS
+    alpha_init = alpha_from_n_func(n_init)    
 
-#    dx = x_vals[1] - x_vals[0]
-#    alpha_padded = torch.cat([alpha_init[:1], alpha_init, alpha_init[-1:]], dim=0)
-#    N_x_init = -(alpha_padded[2:] - alpha_padded[:-2]) / (2.0 * dx)
-    
-
-    # Initial diffusive flux scriptJ(t=0,x)
+    # Initial diffusive flux q(t=0,x)
     d, f, g = 0.05, 10.0, 1.05 # Initial condition parameters
 
     # First setup:
-    scriptJ_init = d * torch.exp(- (f * x_vals / L)**2) + g
+    q_init = d * torch.exp(- (f * x_vals / L)**2) + g
 
     # Second setup:
-    #scriptJ_init = torch.ones_like(x_vals)*g
+    #q_init = torch.ones_like(x_vals)*g
 
     # Third setup:
     #scriptJ_init = 1e-3 * torch.ones_like(x_vals)*g
 
-    return scriptJ_init, alpha_init
+    return q_init, alpha_init
